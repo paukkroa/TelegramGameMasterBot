08e6d0ac0a550487ccc30e0af192d4888bd9b847
Roope Paukku <roope.paukku@gmail.com>
2024-09-28 11:54:57 +0300

    Chat_id based waitlists and sessions

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 sql_connection = db.connect()
-waitlist = Waitlist()
+current_waitlists = dict()
 
 """
 ______ _   _ _   _ _____ _____ _____ _____ _   _  _____ 
@@ -80,6 +80,7 @@ If you have any questions, feel free to ask by tagging me in the group chat (rem
 # TODO: Add command to list all available games
 # TODO: Add command to insert private information about a player (player facts)
 
+# TODO: This might be obsolete with the new /start command
 async def handle_new_member(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Add member into D_PLAYER automatically when they join the group"""
     for member in update.message.new_chat_members:
@@ -93,26 +94,35 @@ async def handle_new_member(update: Update, context: ContextTypes.DEFAULT_TYPE) 
 
         db.insert_player(sql_connection, member.id)
 
-async def handle_join_session(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def handle_join_waitlist(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Add player into waitlist when they send /join """
     user = update.effective_user
+    chat_id = update.effective_chat.id
+
+    if chat_id in current_waitlists.keys():
+        waitlist = current_waitlists[chat_id]
+        logger.info(f"Waitlist exists for chat {chat_id}: {waitlist}")
+    else:
+        waitlist = Waitlist(chat_id)
+        current_waitlists[chat_id] = waitlist
+        logger.info(f"Created new waitlist for chat {chat_id}: {waitlist}")
 
     if waitlist.add_player(user.id):
+        logger.info(f"Player {user.id} added to waitlist in chat {chat_id}")
+        logger.info(f"Current waitlist: {waitlist.player_ids}")
         await update.message.reply_text("Joined session succesfully")
     else:
         await update.message.reply_text("You have already joined the session")
+        logger.info(f"Player {user.id} already in waitlist in chat {chat_id}")
+        logger.info(f"Current waitlist: {waitlist.player_ids}")
 
-    return
-    # TODO: Add actual session id
-    db.add_player_to_session(sql_connection, 1, user.id)
-
-
+# TODO: Delete this command as it is most likely obsolete (replaced by /start)
 async def handle_join_group(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Add player into D_PLAYER if not exists"""
     user_id = update.effective_user.id
     player_exists = False
 
-    existing_players = db.get_players(sql_connection)
+    existing_players = db.get_all_players(sql_connection)
     logger.info(f"Existing players: {existing_players}")
     for player in existing_players:
         if player[0] == user_id:
@@ -126,20 +136,39 @@ async def handle_join_group(update: Update, context: ContextTypes.DEFAULT_TYPE) 
     else:
         await update.message.reply_text("You are already a member of this group")
 
-    # TODO: Create D_GROUP and R_GROUP_PLAYER tables to be able to add player to unique groups
-
 async def list_all_players(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a list of all registered players in databse"""
     # TODO: get from actual session and make response cleaner
     players = db.get_all_players(sql_connection)
     await update.message.reply_text(str(players))
 
+async def list_group_members(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a list of all registered players in the current chat"""
+    # TODO: get from actual session and make response cleaner
+    players = db.get_chat_members(sql_connection, update.effective_chat.id)
+    await update.message.reply_text(str(players))    
+
 async def print_waitlist(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await waitlist.list_players(update, context)
+    chat_id = update.effective_chat.id
+    try:
+        waitlist = current_waitlists[chat_id]
+        logger.info(f"Waitlist found for chat {chat_id}: {waitlist}")
+        await waitlist.list_players(update, context)
+    except KeyError:
+        logger.info(f"No waitlist found for chat {chat_id}")
+        await update.message.reply_text("No waitlist found for the chat. Press /join to join the waitlist.")
 
 async def start_tournament(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    host = update.effective_user
-    session_id = db.start_session(sql_connection, host.id)
+    chat_id = update.effective_chat.id
+    try:
+        waitlist = current_waitlists[chat_id]
+        logger.info(f"Waitlist found for chat {chat_id}: {waitlist}")
+    except KeyError:
+        logger.info(f"No waitlist found for chat {chat_id}")
+        await update.message.reply_text("No players are on the waitlist, cannot start tournament. Press /join to join the waitlist.")
+        return
+    
+    session_id = db.start_session(sql_connection, chat_id)
     # Add extra empty char to list to prevent index out of range error
     msg_words = update.message.text.split(' ') + ['']
 
@@ -150,23 +179,21 @@ async def start_tournament(update: Update, context: ContextTypes.DEFAULT_TYPE) -
 
     logger.info(f"Tournament games: {number_of_games}")
 
-    # TODO: fix? should work though
     for player_id in waitlist.player_ids:
         db.add_player_to_session(sql_connection, session_id, player_id)
 
+    # TODO: test if this works as intended. The session id should keep it in check(?)
     tournament = Tournament(session_id, waitlist.player_ids, number_of_games, update, context, sql_connection, BOT_TG_ID)
     logger.info(f"Tournament {tournament}")
 
-    waitlist.clear()
+    waitlist.clear() # Is this needed anymore?
+    del current_waitlists[chat_id]
     await tournament.start()
-
-    # TODO: implement this when needed
-    # session_players = db.get_session_players(sql_connection, session_id)
 
 async def end_session(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """End the current session"""
-    user = update.effective_user
-    session_id = db.get_most_recent_session_by_player(sql_connection, user.id)
+    chat_id = update.effective_chat.id
+    session_id = db.get_most_recent_session_by_chat(sql_connection, chat_id)
 
     if session_id is None:
         await update.message.reply_text("No active session found to end.")
@@ -175,6 +202,8 @@ async def end_session(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
     db.end_session(sql_connection, session_id)
     await update.message.reply_text(f"Session {session_id} has been ended.")
 
+# TODO: Fix the below functions to work with the current_waitlist dict
+# or delete them completely
 async def handle_number_game_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     game = GuessNumber(id=1, player_ids=waitlist.player_ids, update=update, context=context)
     logger.info(f'Guess number game start')
@@ -277,11 +306,14 @@ def main() -> None:
     # Help command
     application.add_handler(CommandHandler("help", help_command))
     # Join single session
-    application.add_handler(CommandHandler("join", handle_join_session))
+    application.add_handler(CommandHandler("join", handle_join_waitlist))
     # Join group
-    application.add_handler(CommandHandler("addme", handle_join_group))
+    # Commented out because it's not needed with the new /start command
+    # application.add_handler(CommandHandler("addme", handle_join_group))
     # List current session players
     application.add_handler(CommandHandler("all_players", list_all_players))
+    # List group members
+    application.add_handler(CommandHandler("group", list_group_members))
     # Print waitlist
     application.add_handler(CommandHandler("waitlist", print_waitlist))
     # Start tournament / session
