78062a7998ac8c941f2305b461dbcce72fc0e2c6
Roope Paukku <roope.paukku@gmail.com>
2024-10-03 16:47:25 +0300

    Added updater information

--- db/settings_queries.py
+++ db/settings_queries.py
@@ -29,16 +29,18 @@ def get_chat_settings(conn: sqlite3.Connection, chat_id: str) -> dict:
     }
     return settings
 
-def set_chat_to_all_context(conn: sqlite3.Connection, chat_id: str) -> None:
+def set_chat_to_all_context(conn: sqlite3.Connection, chat_id: str, player_id: int) -> None:
     cursor = conn.cursor()
     cursor.execute('''
     UPDATE D_CHAT_SETTINGS
-    SET context_window_type = 'all'
+    SET context_window_type = 'all',
+        uby = ?,
+        udate = CURRENT_TIMESTAMP
     WHERE chat_id = ?
-    ''', (chat_id,))
+    ''', (player_id, chat_id,))
     conn.commit()
 
-def set_chat_to_static_context(conn: sqlite3.Connection, chat_id: str, start_time: str, end_time: str = None) -> None:
+def set_chat_to_static_context(conn: sqlite3.Connection, chat_id: str, player_id: int, start_time: str, end_time: str = None) -> None:
     cursor = conn.cursor()
     if end_time is not None:
         cursor.execute('''
@@ -46,68 +48,84 @@ def set_chat_to_static_context(conn: sqlite3.Connection, chat_id: str, start_tim
         SET context_window_type = 'static',
             static_window_start_time = ?,
             static_window_end_time = ?
+            uby = ?,
+            udate = CURRENT_TIMESTAMP
         WHERE chat_id = ?
-        ''', (start_time, end_time, chat_id))
+        ''', (start_time, end_time, player_id, chat_id))
     else:
         cursor.execute('''
         UPDATE D_CHAT_SETTINGS
         SET context_window_type = 'static',
-            static_window_start_time = ?
+            static_window_start_time = ?,
+            uby = ?,
+            udate = CURRENT_TIMESTAMP
         WHERE chat_id = ?
-        ''', (start_time, chat_id))
+        ''', (start_time, player_id, chat_id))
     conn.commit()
 
-def update_chat_static_context_end_time(conn: sqlite3.Connection, chat_id: str, end_time: str = None) -> None:
+def update_chat_static_context_end_time(conn: sqlite3.Connection, chat_id: str, player_id: int, end_time: str = None) -> None:
     cursor = conn.cursor()
     if end_time is not None:
         cursor.execute('''
         UPDATE D_CHAT_SETTINGS
         SET static_window_end_time = ?
+            uby = ?,
+            udate = CURRENT_TIMESTAMP
         WHERE chat_id = ?
-        ''', (end_time, chat_id))
+        ''', (end_time, player_id, chat_id))
     else:
         cursor.execute('''
         UPDATE D_CHAT_SETTINGS
         SET static_window_end_time = CURRENT_TIMESTAMP
+            uby = ?,
+            udate = CURRENT_TIMESTAMP
         WHERE chat_id = ?
-        ''', (chat_id,))
+        ''', (player_id, chat_id,))
     conn.commit()
 
-def set_chat_to_rolling_context(conn: sqlite3.Connection, chat_id: str, window_size: int) -> None:
+def set_chat_to_rolling_context(conn: sqlite3.Connection, chat_id: str, player_id: int, window_size: int) -> None:
     """Window_size is in seconds"""
     cursor = conn.cursor()
     cursor.execute('''
     UPDATE D_CHAT_SETTINGS
     SET context_window_type = 'rolling',
         rolling_context_window_size = ?
+        uby = ?,
+        udate = CURRENT_TIMESTAMP
     WHERE chat_id = ?
-    ''', (window_size, chat_id))
+    ''', (window_size, player_id, chat_id))
     conn.commit()
 
-def set_chat_to_n_messages_context(conn: sqlite3.Connection, chat_id: str, n_messages: int) -> None:
+def set_chat_to_n_messages_context(conn: sqlite3.Connection, chat_id: str, player_id: int, n_messages: int) -> None:
     cursor = conn.cursor()
     cursor.execute('''
     UPDATE D_CHAT_SETTINGS
     SET context_window_type = 'n-messages',
-        n_messages = ?
+        n_messages = ?,
+        uby = ?,
+        udate = CURRENT_TIMESTAMP
     WHERE chat_id = ?
-    ''', (n_messages, chat_id))
+    ''', (n_messages, player_id, chat_id))
     conn.commit()
 
-def set_chat_to_session_context(conn: sqlite3.Connection, chat_id: str) -> None:
+def set_chat_to_session_context(conn: sqlite3.Connection, chat_id: str, player_id: int) -> None:
     cursor = conn.cursor()
     cursor.execute('''
     UPDATE D_CHAT_SETTINGS
     SET context_window_type = 'session'
+        uby = ?,
+        udate = CURRENT_TIMESTAMP
     WHERE chat_id = ?
-    ''', (chat_id,))
+    ''', (player_id, chat_id,))
     conn.commit()
 
-def set_chat_to_no_context(conn: sqlite3.Connection, chat_id: str) -> None:
+def set_chat_to_no_context(conn: sqlite3.Connection, chat_id: str, player_id: int) -> None:
     cursor = conn.cursor()
     cursor.execute('''
     UPDATE D_CHAT_SETTINGS
     SET context_window_type = 'none'
+        uby = ?,
+        udate = CURRENT_TIMESTAMP
     WHERE chat_id = ?
-    ''', (chat_id,))
+    ''', (player_id, chat_id,))
     conn.commit()
