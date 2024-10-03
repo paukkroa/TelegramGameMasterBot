9ffafa379b51e6671d3c3aa8fd9e83905937be02
Roope Paukku <roope.paukku@gmail.com>
2024-10-03 16:35:10 +0300

    Moved get_chat_settings to a separate 'settings_queries.py' file

--- ai_utils/llm.py
+++ ai_utils/llm.py
@@ -4,7 +4,7 @@ from telegram.ext import ContextTypes
 import sqlite3
 
 from db.session_queries import *
-from db.chat_queries import get_chat_settings
+from db.settings_queries import get_chat_settings
 from ai_utils.llm_utils import LLM_MODEL, SYS_PROMPT_WITH_CONTEXT, SYS_PROMPT_NO_CONTEXT, SYS_PROMPT_WITH_CONTEXT_SESSION_ONLY
 from utils.helpers import get_username_by_id
 from utils.logger import get_logger
--- db/chat_queries.py
+++ db/chat_queries.py
@@ -71,28 +71,3 @@ def get_chat_member_ids(conn: sqlite3.Connection, chat_id: str) -> list:
     member_ids = cursor.fetchall()
     member_ids = [id[0] for id in member_ids]
     return member_ids
-
-def get_chat_settings(conn: sqlite3.Connection, chat_id: str) -> dict:
-    cursor = conn.cursor()
-    cursor.execute('''
-    SELECT context_window_type, 
-        rolling_context_window_size, 
-        static_window_start_time, 
-        static_window_end_time,
-        n_messages
-    FROM D_CHAT_SETTINGS
-    WHERE chat_id = ?
-    ''', (chat_id,))
-    row = cursor.fetchone()
-    if row is None:
-        logger.info(f"No settings found for chat_id {chat_id}.")
-        return None
-
-    settings = {
-        'context_window_type': row[0],
-        'rolling_context_window_size': row[1],
-        'static_window_start_time': row[2],
-        'static_window_end_time': row[3],
-        'n_messages': row[4]
-    }
-    return settings
