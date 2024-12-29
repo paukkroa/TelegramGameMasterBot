50746bf4e646ed621c693b74c93b698b99078fd1
Roope Paukku <roope.paukku@gmail.com>
2024-12-29 21:57:01 +0200

    Refactored AI engines into a separate file

---
+++ ai_utils/engines/llm_service.py
@@ -0,0 +1,43 @@
+from ai_utils.engines.ollama_service import OllamaService
+from utils.config import LLM_ENGINE, LLM_MODEL
+from ai_utils.llm_utils import SYS_PROMPT_WITH_CONTEXT
+
+class LLMService():
+    def __init__(self,
+                 engine: str = LLM_ENGINE,
+                 model: str = LLM_MODEL,
+                 system_prompt: str = SYS_PROMPT_WITH_CONTEXT
+                 ):
+        self.model = model
+        self.engine = self._get_engine(engine)
+        self.system_prompt = system_prompt
+
+    def _get_engine(self, engine: str):
+        """
+        Select the engine to use
+        """
+        if engine == "ollama":
+            return OllamaService(model=self.model)
+        else:
+            raise ValueError("Engine not found")
+        
+    def set_sys_prompt(self, system_prompt: str):
+        """
+        Set the system prompt
+        """
+        self.system_prompt = system_prompt
+        
+    def is_available(self) -> bool:
+        """
+        Check if selected service is available
+        """
+        return self.engine._is_available()
+    
+    def chat(self, message, system_prompt = None) -> str:
+        """ 
+        Respond to a message generically
+        """
+        if system_prompt is None:
+            system_prompt = self.system_prompt
+
+        return self.engine.chat(message, system_prompt)
---
+++ ai_utils/engines/ollama_service.py
@@ -0,0 +1,42 @@
+import ollama
+import requests
+
+class OllamaService():
+    def __init__(self,
+                 model: str = "qwen2.5:14b-instruct"
+                 ):
+        self.engine = ollama
+        self.model = model
+
+    def _is_available(self) -> bool:
+        """
+        Check if Ollama service is available at localhost:11434
+        """
+        try:
+            response = requests.get('http://localhost:11434')
+            if response.status_code == 200:
+                return True
+            else:
+                return False
+        except requests.exceptions.ConnectionError:
+            return False
+
+    def chat(self, message, system_prompt) -> str:
+        """ 
+        Respond to a message generically
+        """
+        if not self._is_available():
+            return "Unfortunately my brain is not available right now. Please try again later."
+
+        response = self.engine.chat(model=self.model, messages=[
+            {
+                'role': 'system',
+                'content': system_prompt,
+            },
+            {
+                'role': 'user',
+                'content': f"{message}",
+            },
+        ])
+
+        return response['message']['content']
